63b5fcafef89ebd3421561907ad578782eb2e795
JordiS <jordisaucedo@gmail.com>
2021-11-29 20:33:39 +0100

    commit

--- call_api.py
+++ call_api.py
@@ -4,11 +4,16 @@ import pandas as pd
 class ApiHandler:
     def __init__(self, day):
         self.day = day 
-        self.url_base = 'https://api.esios.ree.es/archives/70/download_json?locale=es&date='
+        self.url_base = 'https://api.esios.ree.es/archives/70/download_json?locale=es&date=2021-01-01'
         
     
-    def get_ticker_master(self):
-        url = f'{self.url_base}self.day'
+    def get_precio(self):
+        url = f'{self.url_base}'
         response = requests.get(url)
-        df = pd.DataFrame(response.json()[’PVPC’])
+        df = pd.DataFrame(response.json()['PVPC'])
         return df
+
+#
+a1 = ApiHandler(day = '2021-10-01')
+precio = a1.get_precio()
+print(precio)
